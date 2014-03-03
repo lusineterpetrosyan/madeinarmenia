40ff90f6231fcbd5f4ff40a79f4e1e9d2a9a0838
vachi <vache@dendritecorp.com>
2014-03-03 19:26:58 +0400

    dd

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -21,25 +21,26 @@
             });
     	});
 
-
         $(".add_founder").click(function(){
-            add_founder();
-        }); var add_founder = function(){
-                var that = this;
-                if ($('.founder_input').val().length>2){
-                    $(".founders").append(
-                        "<li><h3>"+$('.founder_input').val()+"</h3><button class='remove_founder'>-</button></li>"
-                    );
-                }
-                else{
-                    $('.founder_input').attr("placeholder","please add more shit");
-                }
-                $('.founder_input').val("");
-            };
+		    if ($('.founder_input').val().length>2){
+		        $(".founders").append(
+		            "<li><h3>"+$('.founder_input').val()+"</h3><button class='remove_founder'>-</button></li>"
+		        );
+		    }
+		    else{
+		        $('.founder_input').attr("placeholder","please add more shit");
+		    }
+		    $('.founder_input').val("");            
 
-        $(".remove_founder").click(function(){            
-            $(this).parent().remove();
+
+
+		    $(".remove_founder").click(function(){	            
+	            $(this).parent().remove();
+	        });
+
         }); 
-    });
 
+        
+    });
 }(window.jQuery)
+
